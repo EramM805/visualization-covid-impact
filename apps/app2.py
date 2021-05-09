c5548a0601e9063dac928549939b2da4d8c6c389
Peter Ye <PeterYe100@users.noreply.github.com>
2021-05-08 23:49:52 -0400

    finished heatmap for unemployment rates

--- apps/app2.py
+++ apps/app2.py
@@ -1,37 +1,31 @@
-import pandas as pd
-import plotly.express as px  # (version 4.7.0)
-import plotly.graph_objects as go
-import pathlib
-
-import dash  # (version 1.12.0) pip install dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+import pathlib
+from app import app
 
-app = dash.Dash(__name__)
-
-# get relative data folder
-PATH = pathlib.Path(__file__).parent
-DATA_PATH = PATH.joinpath("../data").resolve()
-dfg = pd.read_csv(DATA_PATH.joinpath("Unemployment-2007-2021.csv"))
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 
+# get relative data folder
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("../data").resolve()
 
+# owner: shivp Kaggle. Source: https://data.mendeley.com/datasets
+# dataset was modified. Original data: https://www.kaggle.com/shivkp/customer-behaviour
+dfg = pd.read_csv(DATA_PATH.joinpath("Unemployment-2007-2021.csv"))
 
-# ------------------------------------------------------------------------------
-# App layout
 layout = html.Div([
-    html.H1('Unemployment Rates by State', style={"textAlign": "center"}),
+    html.H1('Unemployment Rates by State', style={"textAlign": "center", "background": "black", 'color': '#7FDBFF'}),
 
     html.Div([
         html.Div([
-            html.Pre(children="Year", style={"fontSize":"150%"}),
+            html.Pre(children="Year", style={"fontSize":"150%", "background": "black", 'color': '#7FDBFF'}),
             dcc.Dropdown(
                 id='year-dropdown', value='2020', clearable=False,
                 persistence=True, persistence_type='session',
@@ -40,29 +34,35 @@ layout = html.Div([
         ], className='six columns'),
 
         html.Div([
-            html.Pre(children="Month", style={"fontSize": "150%"}),
+            html.Pre(children="Month", style={"fontSize": "150%", "background": "black", 'color': '#7FDBFF'}),
             dcc.Dropdown(
-                id='month-dropdown', value='4', clearable=False,
+                id='month-dropdown', value='3', clearable=False,
                 persistence=True, persistence_type='local',
                 options=[{'label': x, 'value': x} for x in sorted(dfg["Month"].unique())]
             )
             ], className='six columns'),
-    ], className='row'),
+    ], className='row', style={"textAlign": "center", "background": "black", 'color': '#7FDBFF'}),
 
     dcc.Graph(id='my-map', figure={}),
-])
+],className=colors)
+
 
 @app.callback(
     Output(component_id='my-map', component_property='figure'),
     [Input(component_id='year-dropdown', component_property='value'),
      Input(component_id='month-dropdown', component_property='value')]
 )
-def display_value(year_chosen, month_chosen):
+def display_value(pymnt_chosen, month_chosen):
     dfg_fltrd = dfg[(dfg['Month'] == month_chosen) &
-                    (dfg["Year"] == year_chosen)]
-    dfg_fltrd = dfg_fltrd.groupby(["State"])[['Unemployment Rate']].sum()
+                    (dfg["Year"] == pymnt_chosen)]
+    dfg_fltrd = dfg_fltrd.groupby(["State Code", 'State', 'Labor Force Total'])[['Unemployment Rate']].sum()
     dfg_fltrd.reset_index(inplace=True)
-    fig = px.choropleth(dfg_fltrd, locations="State",
-                        locationmode="USA-states", color="Unemployment Rate",
+    fig = px.choropleth(dfg_fltrd, locations="State Code", hover_data=['State','Labor Force Total', 'Unemployment Rate'],
+                        locationmode="USA-states", color="Unemployment Rate", template='plotly_dark', color_continuous_scale=px.colors.sequential.YlOrRd,
                         scope="usa")
+    fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+    )
     return fig
