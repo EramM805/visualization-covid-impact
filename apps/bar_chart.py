56987a143aceac3f1ae001eedad03926d237bc2a
mitchellmui17 <mitchellmui17@gmail.com>
2021-05-08 22:37:12 -0400

    test data

--- apps/bar_chart.py
+++ apps/bar_chart.py
@@ -1,68 +1,70 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
-import plotly.io as pio
 import pandas as pd
+import pathlib
+import statistics
+app = dash.Dash(__name__)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.read_csv("../data/Unemployment-2007-2021.csv")
-df = df[df['Year'] =='2008']
-df = df.groupby(['Month', 'State',],as_index=False)[['Unemployment Total','Unemployment Rate']].sum()
-print (df[:5])
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("../data").resolve()
+df = pd.read_csv(DATA_PATH.joinpath("Unemployment-2007-2021.csv"))
+#years = ["2008", "2009", "2010", "2011", "2012", "2013", "2014","2015", "2016", "2017", "2018", "2019", "2020"]
 
 
-bar_chart = px.bar(
-    data_frame = df,
-    x = 'State',
-    y = 'Unemployment Rate',
-    color = "State",
-    opacity = 0.9,
-    orientation = "v",
-    barmode = 'relative',
+app.layout = html.Div([
+    html.H1("United States Unemployment Statistics 2008 - 2020", style = {"textAlign": "center", "width": "100%", "fontSize": "150%"}),
+    
+    html.Div([
+        
+        
+        html.Div([
+        html.Pre(children="Year", style = {"textAlign": "center", "width": "100px", "fontSize": "150%"}),
+        dcc.Dropdown(
+            style = {"textAlign": "center", "width": "100px"},
+            id="dropdown",
+            options=[{"label": x, "value": x} for x in sorted(df["Year"].unique())],
+            value=2008,
+            clearable=False,
+        )
+        ], className = 'six columns'),
 
-)
-
-bar_chart.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=bar_chart
-    )
+        html.Div([
+        html.Pre(children="States", style = {"textAlign": "center", "width": "100px", "fontSize": "150%"}),
+        dcc.Dropdown(
+            style = {"textAlign": "center", "width": "100px"},
+            id="dropdown-state",
+            options=[{"label": x, "value": x} for x in sorted(df["State"].unique())],
+            value="Alabama",
+            clearable=False,
+        )
+        ], className = 'six columns'),
+    ], className = 'row'),
+    
+    dcc.Graph(id="bar-chart"),
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+@app.callback(
+    Output("bar-chart", "figure"), 
+    [Input("dropdown", "value"),
+    Input("dropdown-state", "value")]
+    )
+def update_bar_chart(year, state):
+    year_df = df[(df['Year'] == year) & (df['State'] == state)]
+    #year_df = year_df.groupby(["Month"])[['Unemployment Total']]
+    fig = px.bar(
+        year_df, 
+        y="Unemployment Total", 
+        x="Month", 
+        hover_data=["State", "Unemployment Rate", "Year", "Month"], 
+        color = "Unemployment Rate",
+        orientation = "v",
+        barmode="group")
+    return fig
+
+app.run_server(debug=True)
--- data_scrape.py
+++ data_scrape.py
@@ -10,9 +10,22 @@ states_data.columns = ['FIPS Code', 'State', 'Year', 'Month', 'Civilian Non-Inst
     'Employment Percent of Population', 'Unemployment Total', 'Unemployment Rate']
 states_data = states_data.iloc[9:]
 
+months = {'1': 'January',
+   '2': 'February',
+   '3': 'March', 
+   '4': 'April',
+   '5': 'May',
+   '6': 'June',
+   '7': 'July',
+   '8': 'August',
+   '9': 'September',
+   '10': 'October',
+   '11': 'November',
+   '12': 'December'}
 # 1st month from 2007-2021
 by_month_data = states_data.loc[(states_data['Year'] > '2007')]
 by_month_data = by_month_data.reset_index()
+by_month_data['Month'] = by_month_data['Month'].map(months).fillna(by_month_data['Month'])
 
 by_month_data.to_csv(path_or_buf = './data/Unemployment-2007-2021.csv')
 
