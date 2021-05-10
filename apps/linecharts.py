19a7dde073d952b13061fb52d99985be7109125f
Eram M <eram.manasia@gmail.com>
2021-05-09 21:28:50 -0400

    finished unemployement us total chart

--- apps/linecharts.py
+++ apps/linecharts.py
@@ -15,12 +15,14 @@ from app import app
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../data").resolve()
 
-df = pd.read_csv(DATA_PATH.joinpath("Unemployment-2007-2021.csv"))
+df = pd.read_csv(DATA_PATH.joinpath("Unemployment-2007-2021(n).csv"))
 fig = px.line(df, x="Year", y="Unemployment Rate")
 
 df_gdp_us = pd.read_csv(DATA_PATH.joinpath("GDP_us.csv"))
 fig2 = px.line(df_gdp_us, x="DATE", y="GDP",template="plotly_dark")
 
+df_unemployement_us = pd.read_csv(DATA_PATH.joinpath("Unemployement-2008-2021-US.csv"))
+fig3 = px.line(df_unemployement_us, x="DATE", y="UNRATE",template="plotly_dark")
 
 fig.update_traces(mode='lines+markers') 
 layout = html.Div(children=[
@@ -31,13 +33,23 @@ layout = html.Div(children=[
             # 'color': colors['text']
         }
     ),
-        html.H3(children='US GDP', style={
+     html.H3(children='US GDP', style={
         # 'color': colors['text']
     }),
     dcc.Graph(
         id='gdp-us-line-chart',
         figure=fig2
     ),
+
+   html.H3(children='US unemployement rate', style={
+        # 'color': colors['text']
+    }),
+    dcc.Graph(
+        id='unemployement-us-line-chart',
+        figure=fig3
+    ),
+
+    
 
     html.H3(children='Unemployement rates per state.', style={
         # 'color': colors['text']
@@ -47,7 +59,7 @@ layout = html.Div(children=[
    html.Div([
             html.Pre(children="Month"),
             dcc.Dropdown(
-                id='month-dropdown', value='1', clearable=False,
+                id='month-dropdown', value='January', clearable=False,
                 persistence=True, persistence_type='local',
                 options=[{'label': x, 'value': x} for x in sorted(df["Month"].unique())],
             )
