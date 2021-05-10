f001f291114830ec2404b2c77f66ee5d5b3bfda9
PETERYE100 <pye000@citymail.cuny.edu>
2021-05-10 15:49:26 -0400

    changes

--- index.py
+++ index.py
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-
-# Connect to main app.py file
-from app import app
-from app import server
-
-# Connect to your app pages
-from apps import app1, app2
-
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div([
-        dcc.Link('App1|', href='/apps/app1'),
-        dcc.Link('Heatmap', href='/apps/app2'),
-    ], className="row", style={"background": "black", 'color': '#7FDBFF'}),
-    html.Div(id='page-content', children=[],style={"background": "black", 'color': '#7FDBFF'})
-])
-
-
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/apps/app1':
-        return app1.layout
-    if pathname == '/apps/app2':
-        return app2.layout
-    else:
-        return "404 Page Error! Please choose a link"
-
-
-if __name__ == '__main__':
-=======
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -74,5 +38,4 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
->>>>>>> 18f61487011c6a7dd04f4bec1ff89a9cc39682f1
     app.run_server(debug=False)
