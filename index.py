4af72de41e4e11fc14b2beb1d22c43f8da204d8a
PETERYE100 <pye000@citymail.cuny.edu>
2021-05-18 20:19:46 -0400

    css styling added

--- index.py
+++ index.py
@@ -7,9 +7,8 @@ from app import app
 from app import server
 
 # Connect to your app pages
-from apps import app1, app2, bar_chart, linecharts, scatterplotmat, scatterplotmat2, stock, linearregression
+from apps import app2, bar_chart, linecharts, scatterplotmat, scatterplotmat2, stock
 
-# from apps import app1, app2, bar_chart, linecharts, scatterplotmat, scatterplotmat2, djia_vis, candlestick, linearregression, stock
 styling = {
     'background': '#111111',
     'text': '#7FDBFF',
@@ -38,7 +37,7 @@ unemployment = 'As the COVID-19 pandemic spread rapidly throughout the world, '\
     'As indicated in the visualizations below, unemployment rates peaked in April of 2020 and '\
     'began to decrease steadily over time.'
 
-stock_str = 'As unemployment rates soared during the COVID-19 pandemic, the U.S. stock market plunged '\
+stock = 'As unemployment rates soared during the COVID-19 pandemic, the U.S. stock market plunged '\
     'as investors hastily sold their holdings in anticipation of worker layoffs. '\
     'Unlike the Great Recession of 2008, stocks plunged between March and April of 2020 for the COVID-19 pandemic. '\
     'During the Great Recession, stocks appeared to have slowly declined over successive months.'
@@ -50,10 +49,8 @@ app.layout = html.Div([
         html.Div([
             dcc.Link('    COVID-19 Impact on U.S. Economy', href='/',  style={"marginLeft": "1rem","fontFamily":"Blippo, fantasy", "textDecoration": "none", "fontSize": '3rem', 'color': '#e2f7ff'})
         ]),
-        dcc.Link('App1', href='/apps/app1', className='button'),
-        dcc.Link('Heatmap', href='/apps/app2', className='button'),
+        dcc.Link('Unemployment', href='/apps/unemployment', className='button'),
         dcc.Link('Line Chart', href='/apps/linecharts', className='button'),
-        dcc.Link('Linear Regression', href='/apps/linearregression', className='button'),
         dcc.Link('BarChart', href='/apps/barchart', className='button'),
         dcc.Link('ScatterPlot', href='/apps/scatterplotmat2', className='button'),
         dcc.Link('Stock', href='/apps/stock', className='button')
@@ -73,7 +70,7 @@ layout = html.Div([
         html.H3('U.S. Unemployment Analysis'),
         html.P(unemployment, style={'color': '#e2f7ff'}),
         html.H3('U.S. Stock Analysis'),
-        html.P(stock_str, style={'color': '#e2f7ff'}),
+        html.P(stock, style={'color': '#e2f7ff'}),
     ], style={'marginTop':'2%'})
     ], style={'padding': '3%'})
 ])
@@ -82,22 +79,16 @@ layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/app1':
-        return app1.layout
     if pathname =='/apps/scatterplotmat':
         return scatterplotmat.layout
     if pathname =='/apps/scatterplotmat2':
         return scatterplotmat2.layout
-    if pathname == '/apps/app2':
+    if pathname == '/apps/unemployment':
         return app2.layout
     if pathname == '/apps/linecharts':
         return linecharts.layout
     if pathname == '/apps/barchart':
         return bar_chart.layout
-    if pathname == '/apps/candlestick':
-        return candlestick.layout
-    if pathname == '/apps/linearregression':
-        return linearregression.layout
     if pathname == '/apps/stock':
         return stock.layout
     if pathname == '/':
