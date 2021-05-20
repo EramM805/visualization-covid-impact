ce7fffbca5c433c49234d43ca8914ca74aacb47b
jamesey123 <jameseyexcaliber@gmail.com>
2021-05-19 20:17:38 -0400

    updated stocks.py

--- apps/djia_vis.py
+++ apps/djia_vis.py
@@ -9,10 +9,8 @@ from app import app
 
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../data").resolve()
-df = pd.read_csv(DATA_PATH.joinpath("DJIA2.0.csv"))
+df = pd.read_csv(DATA_PATH.joinpath("DJI.csv"))
 
-# fig = px.line(df, x="Date", y="High")
-month_selc = df['Month'].unique()
 
 layout = html.Div([
 
@@ -33,78 +31,36 @@ layout = html.Div([
 
     ], style={'width': '48%', 'display': 'inline-block'}),
 
-    html.Div([ 
-        
-        dcc.Dropdown(
-            id='xaxis-column',
-            options=[{'label': i, 'value': i} for i in month_selc],
-            value='Jan',
-            )  
-
-    ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
     
-
     dcc.Graph(id='djia-graphic'),
-    
-    dcc.Graph(id='djia-graphic-2')
-
-   # dcc.Slider(
-        #id='month--slider',
-        #min=1,
-        #max=12,
-        #value=12,
-        #marks={str(month): str(month) for month in df['Month'].unique()},
-        #marks={
-        #1: 'Jan',
-        #2: 'Feb',
-        #3: 'Mar',
-        #4: 'Apr',
-        #5: 'May',
-        #6: 'jun',
-        #7: 'jul',
-        #8: 'Aug',
-        #9: 'Sep',
-        #10: 'Oct',
-        #11: 'Nov',
-        #12: 'Dec'
-
-    #},
-        #step=None,
-    #)
-
+    dcc.Graph(id='djia-graphic-2'),
+    dcc.Graph(id='djia-graphic-3'),
+    #dcc.Graph(id='djia-grapics-4')
 
 ])
 
 @app.callback(
     [Output(component_id='djia-graphic', component_property='figure'),
-    Output(component_id='djia-graphic-2', component_property='figure')],
-    [Input(component_id='yaxis-column', component_property='value'),
-    Input(component_id='xaxis-column', component_property='value')])
-    #Input('month--slider', 'value'))
-def update_graph(yaxis_column_name, xaxis_column_name):
-    # x column name is month
-    # y column name is other
-    # df = df[df['Month'] == xaxis_column_name]
-    # df =
-    # dff = df[df['Month'] == xaxis_column_name]
-    # dff_2020 = df['Year'] = 2020
-    print(df[df['Year'] == 2020])
-    dff_2020 = df[(df['Month'] == xaxis_column_name) & (df['Year'] == 2020)]
-    dff_2020 = dff_2020[[yaxis_column_name, 'Month', 'Date', 'Year']] 
-    dff_2008 = df[(df['Month'] == xaxis_column_name) & (df['Year'] == 2008)]
-    dff_2008 = dff_2008[[yaxis_column_name, 'Month', 'Date', 'Year']] 
-    # dff = df[(df['Month'] == xaxis_column_name) &
-    #                 (df[] == yaxis_column_name)]
-    print(dff_2020)
-    print(dff_2008)
+    Output(component_id='djia-graphic-2', component_property='figure'),
+    Output(component_id='djia-graphic-3', component_property='figure')
+    #Output(component_id='djia-graphic-4', component_property='figure')
+    ],
+    [Input(component_id='yaxis-column', component_property='value')])
     
-    
-    #dropdown_optons['Open', 'Hign', 'Low', 'Close', 'Adj Close', 'Volume']
+def update_graph(yaxis_column_name):
 
-    # fig = px.line(df,x=df[df['Month'] == xaxis_column_name]['Date'], y=df['High'])
-    
-    #fig.update_layout()
-    fig = px.line(dff_2020, x="Date", template="plotly_dark", y=yaxis_column_name)
-    fig2 = px.line(dff_2008, x="Date", template="plotly_dark", y=yaxis_column_name)
+    #dff_2020 = df[df['Year'] == 2020]
+    #dff_2020 = dff_2020[[yaxis_column_name, 'Date', 'Year']] 
 
-    return [fig, fig2]
+    dff_2020 = df[df['Date'].str.contains("2020")] 
+    dff_2008 = df[df['Date'].str.contains("2009")] 
+
+    fig = px.line(df, x="Date", template="plotly_dark", y=yaxis_column_name)
+    fig2 = px.line(dff_2020, x="Date", template="plotly_dark", y=yaxis_column_name)
+    fig3 = px.line(dff_2008, x="Date", template="plotly_dark", y=yaxis_column_name)
+    #fig4 = px.scatter_matrix(components, labels=labels, dimensions=range(6), color=df['Date'])
+    
+    #fig4.update_traces(diagonal_visible=False)
+
+
+    return [fig, fig2, fig3]
